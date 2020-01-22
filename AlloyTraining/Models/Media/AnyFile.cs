7ae4d714807785786ce4a5ac720a79e77b354f02
Marius Setalid <marius.setalid@gmail.com>
2020-01-22 14:13:04 +0100

    Finished exercise B2

---
+++ AlloyTraining/Models/Media/AnyFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "Any File", GUID = "1069c854-7f20-4efc-bed8-15e9bdd02910", Description = "Use this to upload any type of file.")]
+    /*[MediaDescriptor(ExtensionString = "pdf,doc,docx")]*/
+    public class AnyFile : MediaData
+    {
+    }
+}
